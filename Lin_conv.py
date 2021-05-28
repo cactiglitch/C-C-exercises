b40d6b1d20e8ad6cd363c8c245883067d2ad05c0
cactiglitch <84656334+cactiglitch@users.noreply.github.com>
2021-05-28 14:11:47 +0800

    Update Lin_conv.py
    Linear convection exercises - modify comments

--- Lin_conv.py
+++ Lin_conv.py
@@ -2,24 +2,24 @@ import numpy
 from matplotlib import pyplot      
 import time, sys
 
-nx = 41  # try changing this number from 41 to 81 and Run All ... what happens?
+nx = 41  
 dx = 2 / (nx-1)
-nt = 25    #nt is the number of timesteps we want to calculate
-dt = .025  #dt is the amount of time each timestep covers (delta t)
+nt = 25    
+dt = .025  
 c = 1 
 
-u = numpy.ones(nx)      #numpy function ones()
-u[int(.5 / dx):int(1 / dx + 1)] = 2  #setting u = 2 between 0.5 and 1 as per our I.C.s
+u = numpy.ones(nx)      
+u[int(.5 / dx):int(1 / dx + 1)] = 2 
 print(u)
 
 pyplot.plot(numpy.linspace(0, 2, nx), u);
 
-un = numpy.ones(nx) #initialize a temporary array
+un = numpy.ones(nx)
 
-for n in range(nt):  #loop for values of n from 0 to nt, so it will run nt times
-    un = u.copy() ##copy the existing values of u into un
-    #for i in range(1, nx): ## you can try commenting this line and...
-    for i in range(nx): ## ... uncommenting this line and see what happens!
+for n in range(nt):  
+    un = u.copy() 
+    #for i in range(1, nx): ## alternate
+    for i in range(nx): 
         u[i] = un[i] - c * dt / dx * (un[i] - un[i-1])
             
 pyplot.plot(numpy.linspace(0, 2, nx), u);
