6772c57881b76357841c56ee03561ed35acfa8ef
cactiglitch <84656334+cactiglitch@users.noreply.github.com>
2021-06-10 22:58:08 +0800

    Create 4.2_inline.cpp

---
+++ cpp/4.2_inline.cpp
@@ -0,0 +1,20 @@
+// put contents of function where function is invoked
+#include <iostream>
+
+using namespace std;
+
+inline long DoubleNum(int inputNum)
+{
+	return inputNum * 2;
+}
+
+int main()
+{
+	cout << "Enter an integer: ";
+	int inputNum = 0;
+	cin >> inputNum;
+	
+	cout << "Double is: " << DoubleNum(inputNum) << endl;
+	
+	return 0;
+} 
