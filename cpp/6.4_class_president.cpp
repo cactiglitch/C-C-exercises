cb6d1e7847e88607374cf93d49bf86a34d4c9857
cactiglitch <84656334+cactiglitch@users.noreply.github.com>
2021-06-21 14:17:16 +0800

    Create 6.4_class_president.cpp

---
+++ cpp/6.4_class_president.cpp
@@ -0,0 +1,41 @@
+/*The name of the President is: Abraham Lincoln */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class President
+{
+private:
+	President() {};
+	President(const President&);
+	const President& operator = (const President&);
+	
+	string name;
+	
+public:
+	static President& GetInstance()
+	{
+		//static obects are constructed only once
+		static President onlyInstance;
+		return onlyInstance;
+	}
+	
+	string GetName()
+	{ return name; }
+	
+	void SetName(string InputName)
+	{ name = InputName; }
+};
+
+int main()
+{
+	President& onlyPresident = President::GetInstance();
+	onlyPresident.SetName("Abraham Lincoln");
+	
+	cout << "The name of the President is: ";
+	cout << President::GetInstance().GetName() << endl;
+	
+	 return 0;
+ }
