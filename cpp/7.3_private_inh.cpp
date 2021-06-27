e229ced6846560eb55179c6e6674d608287e0abb
cactiglitch <84656334+cactiglitch@users.noreply.github.com>
2021-06-27 11:42:06 +0800

    Create 7.3_private_inh.cpp

---
+++ cpp/7.3_private_inh.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+class Motor
+{
+public:
+	void SwitchIgnition()
+	{
+		cout << "Ignition ON" << endl;
+	}
+	void PumpFuel()
+	{
+		cout << "Fuel in cylinders" << endl;
+	}
+	void FireCylinders()
+	{
+		cout << "Vrooooom" << endl;
+	}
+};
+
+class Car: private Motor // private inheritance
+{
+public:
+	void Move()
+	{
+		SwitchIgnition();
+		PumpFuel();
+		FireCylinders();
+	}
+};
+
+int main()
+{
+	Car myDreamCar;
+	myDreamCar.Move();
+	return 0;
+}
